44fbcc6694497c330f31155013b43e079b4c63f2
froggagul <froggagul@gmail.com>
2023-07-04 13:51:34 +0900

    fix: mmoe main

--- models/mmoe/main.py
+++ models/mmoe/main.py
@@ -3,21 +3,21 @@ from ensemble import ensemble
 
 # submission-a510f6e-60-sigmoid-ensemble.csv - 6.05774
 
-# os.system(
-#     "python file1.py"
-# )
+os.system(
+    "python file1.py"
+)
 
 # submission-4ba3b2b-sigmoid-ensemble.csv - 6.05728
-# os.system(
-#     "python file2.py"
-# )
+os.system(
+    "python file2.py"
+)
 
 # submission-419419b-447-sigmoid-ensemble.csv - 6.057216
 # submission-419419b-436-sigmoid-ensemble.csv - 6.059001
 # submission-419419b-457-sigmoid-ensemble.csv - 6.056077
-# os.system(
-#     "python file3.py"
-# )
+os.system(
+    "python file3.py"
+)
 
 # ensemble Sigmoid - 6.049729
 ensemble(
